469cbfc3720701dd12e93a5cffee324b369bedd4
Jack Thomas-Colwell <jack.thomascolwell@gmail.com>
2024-07-04 14:08:01 -0400

    Added preset ligands

--- src/presets/LigandEnergies.ts
+++ src/presets/LigandEnergies.ts
@@ -16,9 +16,49 @@ const presetLigandEnergies: Array<'divider' | {name: string, esigma?: string, ep
     },
     'divider',
     {
-        name: 'water (placeholder)',
-        esigma: '1',
+        name: 'H20',
+        esigma: '7490',
+        epi: '1390'
+    },
+    {
+        name: 'OH-',
+        esigma: '7510',
+        epi: '1400'
+    },
+    {
+        name: 'NH3',
+        esigma: '7010',
         epi: '0'
-    }
+    },
+    {
+        name: 'CN-',
+        esigma: '8310',
+        epi: '-290'
+    },
+    {
+        name: 'py',
+        esigma: '5800',
+        epi: '-580'
+    },
+    {
+        name: 'F-',
+        esigma: '7870',
+        epi: '1880'
+    },
+    {
+        name: 'Cl-',
+        esigma: '5610',
+        epi: '910'
+    },
+    {
+        name: 'Br-',
+        esigma: '5050',
+        epi: '690'
+    },
+    {
+        name: 'I-',
+        esigma: '4140',
+        epi: '670'
+    },
 ]
 export default presetLigandEnergies;
